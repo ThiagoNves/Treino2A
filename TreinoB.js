a5c99496270f649eeed2da2392af4dc3c4a3d8f9
ThiagoNves <thiagooliveraer@gmail.com>
2026-05-21 20:05:24 -0300

    Tarefas de Treino - TarefaA2 e TarefaB2

--- TreinoB.js
+++ TreinoB.js
@@ -1,33 +1,17 @@
-const lista1=[];
-    lista1.Tarefa = prompt("Concluiu a Tarefa 1?");
-    lista1.Tarefa = prompt("Concluiu a Tarefa 2?");
-    lista1.Tarefa = prompt("Concluiu a Tarefa 3?");
-    lista1.Tarefa = prompt("Concluiu a Tarefa 4?");
-    lista1.Tarefa = prompt("Concluiu a Tarefa 5?");
+let tarefas = [];
+ 
+let qtdProdutos=0, contador=0
+ 
+qtdProdutos = Number(prompt("Quantas tarefas deseja adicionar?"));
+ 
+do {
+    tarefas[contador] = prompt ("Digite a"+(contador+1)+"º tarefa");
+    alert("Okay, anotado!");
+    contador++
+} while (contador != qtdProdutos);
+ 
+alert ("Suas tarefas são: " + tarefas + ".");
 
-    alert( "Todas as Tarefas concluidas, parabens!!");
-
-
-    
-let Tarefas1={
-    numero1: "",
-    numero2: "Este é ",
-    numero3: "Este é o numero 10",
-    numero4: "Este é o numero 12",
-    numero5: "Este é o numero 16",
+for(let i = 0; i < tarefas.length; i++) {
+    console.log((i + 1) + " - " + tarefas[i]);
 }
-console.log(Tarefas1.numero1)
-
-
-let Tarefas={
-Tarefas1: "Concluida Tarefa 1!!",
-Tarefas2: "Concluida Tarefa 2!!",
-Tarefas3: "Concluida Tarefa 3!!",
-Tarefas4: "Concluida Tarefa 4!!",
-Tarefas5: "Concluida Tarefa 5!!",
-}
-console.log( Tarefas.Tarefas1)
-console.log( Tarefas.Tarefas2)
-console.log( Tarefas.Tarefas3)
-console.log( Tarefas.Tarefas4)
-console.log( Tarefas.Tarefas5)
