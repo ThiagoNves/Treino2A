7d17c9d1f6080d6471dc1c78f9721fae3ac4f0fe
ThiagoNves <thiagooliveraer@gmail.com>
2026-05-21 19:52:46 -0300

    Tarefas de Treino 2

---
+++ TreinoB.js
@@ -0,0 +1,33 @@
+const lista1=[];
+    lista1.Tarefa = prompt("Concluiu a Tarefa 1?");
+    lista1.Tarefa = prompt("Concluiu a Tarefa 2?");
+    lista1.Tarefa = prompt("Concluiu a Tarefa 3?");
+    lista1.Tarefa = prompt("Concluiu a Tarefa 4?");
+    lista1.Tarefa = prompt("Concluiu a Tarefa 5?");
+
+    alert( "Todas as Tarefas concluidas, parabens!!");
+
+
+    
+let Tarefas1={
+    numero1: "",
+    numero2: "Este é ",
+    numero3: "Este é o numero 10",
+    numero4: "Este é o numero 12",
+    numero5: "Este é o numero 16",
+}
+console.log(Tarefas1.numero1)
+
+
+let Tarefas={
+Tarefas1: "Concluida Tarefa 1!!",
+Tarefas2: "Concluida Tarefa 2!!",
+Tarefas3: "Concluida Tarefa 3!!",
+Tarefas4: "Concluida Tarefa 4!!",
+Tarefas5: "Concluida Tarefa 5!!",
+}
+console.log( Tarefas.Tarefas1)
+console.log( Tarefas.Tarefas2)
+console.log( Tarefas.Tarefas3)
+console.log( Tarefas.Tarefas4)
+console.log( Tarefas.Tarefas5)
